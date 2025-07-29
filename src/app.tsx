bbc5675ef98f815c644ba2f0b7dfe6bca53a6a61
salasaa <akhirudinsalasa.gc@gmail.com>
2025-07-29 13:31:14 +0700

    feat: add conditional rendering for status task

--- src/app.tsx
+++ src/app.tsx
@@ -4,17 +4,27 @@ export function App() {
       <h1>Cartify</h1>
       <button>Click me</button>
 
-      <CartList name="Grocery List" />
+      <CartList name="Grocery List" isCompleted />
       <CartList name="Shopping List" />
       <CartList name="Workshop List" />
     </div>
   );
 }
 
-export function CartList({ name }: { name: string }) {
+export function CartList({
+  name,
+  isCompleted,
+}: {
+  name: string;
+  isCompleted?: boolean;
+}) {
   return (
     <div>
-      <h2>{name}</h2>
+      <h2>
+        {name} {isCompleted && "✔️"}
+      </h2>
+      {isCompleted && <p>Task is completed.</p>}
+      {!isCompleted && <p>1 of 7 tasks are completed.</p>}
     </div>
   );
 }
