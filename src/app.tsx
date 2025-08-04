a2991ff91da4175ddeca5a232e6d89beaa962251
salasaa <akhirudinsalasa.gc@gmail.com>
2025-08-05 02:40:38 +0700

    feat: move cartifyList data to a separate module

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-
-const cartifyList = [
-  {
-    id: 1,
-    name: 'Grocery List',
-    isCompleted: true,
-    items: [
-      { id: 1, name: 'Coffee', isCompleted: true },
-      { id: 2, name: 'Milk', isCompleted: true },
-      { id: 3, name: 'Bread', isCompleted: true },
-    ],
-  },
-  {
-    id: 2,
-    name: 'Shopping List',
-    isCompleted: true,
-    items: [
-      { id: 1, name: 'Tshirt', isCompleted: true },
-      { id: 2, name: 'Jeans', isCompleted: false },
-      { id: 3, name: 'Hat', isCompleted: false },
-    ],
-  },
-  { id: 3, name: 'Workshop List', isCompleted: false, items: [] },
-];
+import { cartifyList } from './modules/cartify/data';
 
 export function App() {
   const calculatedLists = cartifyList.map((list) => {
---
+++ src/modules/cartify/data.ts
@@ -0,0 +1,23 @@
+export const cartifyList = [
+  {
+    id: 1,
+    name: 'Grocery List',
+    isCompleted: true,
+    items: [
+      { id: 1, name: 'Coffee', isCompleted: true },
+      { id: 2, name: 'Milk', isCompleted: true },
+      { id: 3, name: 'Bread', isCompleted: true },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Shopping List',
+    isCompleted: true,
+    items: [
+      { id: 1, name: 'Tshirt', isCompleted: true },
+      { id: 2, name: 'Jeans', isCompleted: false },
+      { id: 3, name: 'Hat', isCompleted: false },
+    ],
+  },
+  { id: 3, name: 'Workshop List', isCompleted: false, items: [] },
+];
