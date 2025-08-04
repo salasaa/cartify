ac36e861c070acf1c558094d06d469aa338cee3c
salasaa <akhirudinsalasa.gc@gmail.com>
2025-08-04 23:12:42 +0700

    feat: add Prettier and Tailwind CSS plugin for code formatting

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,34 @@
 const cartifyList = [
   {
     id: 1,
-    name: "Grocery List",
+    name: 'Grocery List',
     isCompleted: true,
     items: [
-      { id: 1, name: "Coffee", isCompleted: true },
-      { id: 2, name: "Milk", isCompleted: true },
-      { id: 3, name: "Bread", isCompleted: false },
+      { id: 1, name: 'Coffee', isCompleted: true },
+      { id: 2, name: 'Milk', isCompleted: true },
+      { id: 3, name: 'Bread', isCompleted: false },
     ],
   },
   {
     id: 2,
-    name: "Shopping List",
+    name: 'Shopping List',
     isCompleted: true,
     items: [
-      { id: 1, name: "Tshirt", isCompleted: true },
-      { id: 2, name: "Jeans", isCompleted: true },
-      { id: 3, name: "Hat", isCompleted: false },
+      { id: 1, name: 'Tshirt', isCompleted: true },
+      { id: 2, name: 'Jeans', isCompleted: true },
+      { id: 3, name: 'Hat', isCompleted: false },
     ],
   },
-  { id: 3, name: "Workshop List", isCompleted: false, items: [] },
+  { id: 3, name: 'Workshop List', isCompleted: false, items: [] },
 ];
 
 export function App() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-200 text-gray-900 dark:text-gray-100 p-4 sm:p-6 md:p-8">
-      <div className="max-w-xl mx-auto flex flex-col min-h-[calc(100vh-2rem)] sm:min-h-[calc(100vh-3rem)]">
-        <header className="flex justify-between items-center mb-6">
+    <div className="min-h-screen bg-white p-4 text-gray-900 transition-colors duration-200 sm:p-6 md:p-8 dark:bg-gray-900 dark:text-gray-100">
+      <div className="mx-auto flex min-h-[calc(100vh-2rem)] max-w-xl flex-col sm:min-h-[calc(100vh-3rem)]">
+        <header className="mb-6 flex items-center justify-between">
           <div className="flex items-end space-x-2">
-            <h1 className="text-2xl sm:text-3xl font-bold font-['Inter']">
+            <h1 className="font-['Inter'] text-2xl font-bold sm:text-3xl">
               Cartify
             </h1>
             <p className="text-sm text-gray-500 dark:text-gray-400">
@@ -36,7 +36,7 @@ export function App() {
             </p>
           </div>
         </header>
-        <ul>
+        <ul className="flex flex-col space-y-4">
           {cartifyList.map((list) => {
             return (
               <li key={list.id}>
@@ -58,9 +58,9 @@ export function CartList({
   isCompleted?: boolean;
 }) {
   return (
-    <div className="p-4 mb-4 bg-white dark:bg-gray-800 rounded-lg shadow-md transition-shadow duration-200 hover:shadow-lg">
-      <h2 className="text-xl font-semibold mb-2">
-        {name} {isCompleted && "✔️"}
+    <div className="mb-4 rounded-lg bg-white p-4 shadow-md transition-shadow duration-200 hover:shadow-lg dark:bg-gray-800">
+      <h2 className="mb-2 text-xl font-semibold">
+        {name} {isCompleted && '✔️'}
       </h2>
       {isCompleted && <p>Task is completed.</p>}
       {!isCompleted && <p>1 of 7 tasks are completed.</p>}
