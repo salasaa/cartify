42cc5de6f4d27f3748235921c39dbcb190cdf26e
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-12 20:15:29 +0700

    day 21

--- src/modules/list/data.ts
+++ src/modules/list/data.ts
@@ -10,6 +10,7 @@ export type DataList = {
   id: number;
   name: string;
   isCompleted: boolean;
+  statusText: string;
   items: ListItem[];
 };
 
@@ -17,7 +18,8 @@ export const dataLists: DataList[] = [
   {
     id: 1,
     name: 'Grocery List',
-    isCompleted: true,
+    isCompleted: true, // buat semuanya udah oke ada centang
+    statusText: 'Task is completed.',
     items: [
       { id: 1, name: 'Coffee', isCompleted: true, quantity: 50, unit: 'gr' },
       { id: 2, name: 'Milk', isCompleted: true, quantity: 3, unit: 'L' },
@@ -28,6 +30,7 @@ export const dataLists: DataList[] = [
     id: 2,
     name: 'Shopping List',
     isCompleted: true,
+    statusText: '2 of 3 tasks are completed.',
     items: [
       { id: 1, name: 'Tshirt', isCompleted: true, quantity: 2, unit: 'pcs' },
       { id: 2, name: 'Jeans', isCompleted: false, quantity: 1, unit: 'pcs' },
@@ -38,9 +41,17 @@ export const dataLists: DataList[] = [
     id: 3,
     name: 'Workshop List',
     isCompleted: false,
+    statusText: '1 of 2 tasks are completed.',
     items: [
       { id: 1, name: 'nails', isCompleted: true, quantity: 1, unit: 'kg' },
       { id: 2, name: 'glue', isCompleted: false, quantity: 1, unit: 'pcs' },
     ],
+  },
+  {
+    id: 4,
+    name: 'New List',
+    isCompleted: false, // buat semuanya udah oke ada centang
+    statusText: '',
+    items: [],
   },
 ];
